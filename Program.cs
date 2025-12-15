666c1c3d7afd561bff23e91690f261be6d2a398d
Niklas Smith <niklas_smith@hotmail.com>
2025-12-15 09:05:01 +0100

    Ground for projekt c#. hot or cold app

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+// projekt hot or cold c# av Niklas Smith, nism2400
+
+namespace hotorcold
+{
+class Program
+{
+    static void Main(string[]args ) {}
+
+
+
+
+
+
+
+
+}
+}
