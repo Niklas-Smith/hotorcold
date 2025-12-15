19fd34a03d2b3e24f0a2f2f29d2fbead203dddbf
Niklas Smith <niklas_smith@hotmail.com>
2025-12-15 18:24:46 +0100

    Made it so you can press 1/2/x to chosse option

--- Program.cs
+++ Program.cs
@@ -7,13 +7,59 @@ namespace hotorcold
 {
 class Program
 {
-    static void Main(string[]args ) {}
+    static void Main(string[]args ) {
+
+
+  while (true)
+  {
+      // rensar 
+      Clear();
+      // gör att cursor är dold.
+    CursorVisible = false;
+  
+      WriteLine("Hot Or Cold");
+   // Ger de här alternativen när programet startar
+   WriteLine("1. Spela hot Or cold");
+ WriteLine("2. Se resultat");
+ WriteLine("X. Avslute spel");
+ 
+// Gör att man kan välja med knapp ett av alternative 1,2 eller x
+ int keyPress = (int)ReadKey(true).Key;
+
+switch(keyPress)
+{
+ case '1':
+
+
+
+
+
+ break; 
+case '2':
+
+break; 
+case 88:
+
+// Gör så man kan avsluta spelet med x
+ Environment.Exit(0);
+break;
+
+  }
 
 
 
 
 
 
+
+
+  }
+
+
+
+
+
+}
 
 
 }
